544e23d2aa42dc12bc6f52d305715fd99178f9b3
Witold Swiatowy <swiatowywitold@gmail.com>
2022-05-21 09:49:56 +0200

    usuniecie nieuzywanych metod i zmiany EmployeeRepository

--- src/main/java/xyz/kuklake/repository/EmployeeRepository.java
+++ src/main/java/xyz/kuklake/repository/EmployeeRepository.java
@@ -2,14 +2,20 @@ package xyz.kuklake.repository;
 
 import xyz.kuklake.employee.Employee;
 
+import java.util.HashMap;
+import java.util.Map;
+
+//save, update, find, delete
 public class EmployeeRepository {
 
-    public Employee find(long id) {
-        Employee employee = new Employee();
-        employee.setId(id);
-        employee.setFirstName("Kuklake" + id);
-        employee.setLast_name("Bulake" + id);
+    private static Map <Long, Employee> employeeMap = new HashMap<>();
 
+    public Employee find(long id) {
+        Employee employee = employeeMap.get(id);
         return employee;
+    }
+
+    public void save(Employee employee){
+        employeeMap.put(employee.getId(), employee);
     }
 }
